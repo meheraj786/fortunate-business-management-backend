9ed0d61a0543401f89ac4056b6b379e8a0918640
meheraj786 <meherajhosen786@gmail.com>
2025-10-13 16:30:38 +0600

    user model add

--- models/user.model.js
+++ models/user.model.js
@@ -1,4 +1,6 @@
 const mongoose = require("mongoose");
+const bcrypt=require("bcrypt")
+const jwt=require("jsonwebtoken")
 
 const userSchema = new mongoose.Schema(
   {
@@ -20,10 +22,38 @@ const userSchema = new mongoose.Schema(
     },
     access: [{
       type: String,
-      default: null
+      default: null,
+      enum: ["admin", "sales", "accounts", "customers"]
+    }],
+    warehouse: [{
+      type: String,
+      default: null,
     }],
   },
 );
+
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
+  this.password = await bcrypt.hash(this.password, 10);
+  next();
+});
+
+userSchema.methods.isPasswordCorrect = async function (password) {
+  return await bcrypt.compare(password, this.password);
+};
+
+userSchema.methods.generateToken = function () {
+  return jwt.sign(
+    {
+      _id: this._id,
+      email: this.email,
+    },
+    process.env.ACCESS_TOKEN_SECRET,
+    {
+      expiresIn: process.env.ACCESS_TOKEN_EXPIRY  
+    }
+  )
+};
 
 const User = mongoose.model("User", userSchema);
 
