e5cc8886865078d81adf8762d1cfd26bcdb4e6aa
Abdullah Al Ashrafee <dev.ashrafee@gmail.com>
2025-11-01 20:58:48 +0600

    Update CORS configuration to use environment variable and modify sample.env

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const cookieParser = require("cookie-parser");
 
 const app = express();
 
-app.use(cors({ origin: "http://localhost:5173" }));
+app.use(cors({ origin: process.env.CORS_ORIGIN }));
 app.use(helmet());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
